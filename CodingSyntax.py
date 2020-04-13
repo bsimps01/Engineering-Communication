baa4b84daafc8f6006f990cd19c377e6c8b8c84a
Ben Simpson <b.albertsimpson@gmail.com>
2020-04-13 02:17:42 -0700

    updates

--- CodingSyntax.py
+++ CodingSyntax.py
@@ -2,17 +2,16 @@
 
 def listDuplicates(list):
     list = [12, 5, 14, 21, 8, 24, 32]
-	dict = { }
+    dict = {} #brackets needed to be closer together
 
-	for i in list:
-		if list[i] in dict:
-			return true
-		else:
-			dict[list[i]] += 1
+    for i in list:
+        if list[i] in dict:
+            return True #capitalizing true
+        else:
+            dict[list[i]] += 1
+    return False # capitalizing here too
 
-	return false
-
-#print(listDuplicates())
+print(listDuplicates(list)) #function needed to be called
 
 #Find the longest substring of unique letters in a given string of n letters.
 
@@ -20,10 +19,9 @@ x = "abccdeffgaabdklmopgreat"
 
 sub = x[0]
 
-long = sub, i
+long, length = sub, 1 #needed to add length to problem
 
 for i in sub:
-    if len(sub[i] <= len(i)):
+    if len(sub[-1]) <= len(i):
         sub += i
-    print(sub
-    )
+    print(sub) #parenthesis needed to be put closer together
